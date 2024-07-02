bef588083d6152e145fd4c403335d80dac578cd8
Vijay Maurya <vijaymaurya912082@gmail.com>
2024-07-03 00:36:48 +0530

    first commit

---
+++ src/main/java/com/Bitespeed/IdentityReconciliation/IdentityReconciliationApplication.java
@@ -0,0 +1,13 @@
+package com.Bitespeed.IdentityReconciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityReconciliationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityReconciliationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Bitespeed/IdentityReconciliation/IdentityReconciliationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bitespeed.IdentityReconciliation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityReconciliationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
